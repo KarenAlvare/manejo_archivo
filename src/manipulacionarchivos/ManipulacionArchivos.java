fbb5a1becda4a042b9a8af6e3fb1d1b225dcb9a9
KarenAlvare <alvarezpalafoxanakaren@gmail.com>
2023-10-10 16:35:16 +0100

    Method that read a file into a String array

--- src/manipulacionarchivos/ManipulacionArchivos.java
+++ src/manipulacionarchivos/ManipulacionArchivos.java
@@ -10,38 +10,74 @@ import java.io.InputStreamReader;
 public class ManipulacionArchivos {
     
     
-    public static void leerArchivos(String name){
-        File archivo; // apunta a un archivo fisico del dd
-        FileReader reader; // llave con permiso de solo lectura
-        BufferedReader bufer; // recuperar info. del archivo
-        String linea;
+    public static int countFileLines(String name){
+        File archivo; //apunta a un archivo fisico del dd
+        FileReader reader; //llave con permiso de solo lectura
+        BufferedReader bufer; //recuperar info. del archivo
+        int numLineas = 0;
         
-        try {
-            //crear un apuntadoral archivo fisico
-            archivo = new File("C:\\Archivos\\" + name + ".txt");
+        try{
+            //crear un apuntador al archivo fisico
+            archivo = new File("C:\\archivos\\" + name + ".txt");          
+            reader = new FileReader(archivo);            
+            bufer = new BufferedReader(reader);
+            //contar las lineas que tiene el archivo
+            while((bufer.readLine())!= null ){
+                numLineas++;
+            }
+            reader.close();
+        } catch( Exception e ){
+            System.out.println("Error al leer el archivo: "+ e.toString());
+        }
+        
+        return numLineas;
+    }
+    
+    public static String[] fileToStringArray(String name) throws IOException{
+        File archivo; //apunta a un archivo fisico del dd
+        FileReader reader; //llave con permiso de solo lectura
+        BufferedReader bufer; //recuperar info. del archivo
+        String linea;
+        String[] array = null;
+        int t;
+        int i = 0;
+        
+        try{
+            //obtener tamaño archivo
+            t = countFileLines(name);
+            //con el tamaño del archivo, contruimos arreglos
+            array = new String[t];
+            //crear un apuntador al archivo fisico
+            archivo = new File("C:\\archivos\\" + name + ".txt");
             //abrir el archivo para lectura
             reader = new FileReader(archivo);
             //configurar el bufer para leer datos del archivo
             bufer = new BufferedReader(reader);
-            //lecturas del contenido del archivo
+            //lectura de contenido del archivo
             while((linea = bufer.readLine())!= null ){
-                System.out.println("Linea leida: "+ linea);
+                //System.out.println("Linea leida: "+ linea);
+                array[i] = linea;
+                i++;
         }
             reader.close();
-        }   catch( Exception e ){
+        } catch( Exception e ){
             System.out.println("Error al leer el archivo: "+ e.toString());
         }
+        return array;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
         
         String fileName;
+        String[] mascotass;
         
-        System.out.println("Lecture de un archivo de texto");
+        System.out.println("Lectura de un archivo de texto");
         System.out.println("Escribe el nombre del archivo: ");
         fileName = bufer.readLine();
-        leerArchivos(fileName);
-    }
-    
-}
+        mascotass = fileToStringArray(fileName);   
+        System.out.println("Contenido del arreglo de mascotas: ");
+        for (String unaMascota : mascotass ){
+            System.out.println(unaMascota);
+        }
+}}
